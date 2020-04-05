fa85440601358528e8d434da1a645f3f0837304b
Mahmoud Adas <mido3ds@gmail.com>
2020-04-05 04:27:21 +0200

    init io

---
+++ src/io.vhdl
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity io is
+    port (
+        in_state: in std_logic_vector(1 downto 0);
+        clk: in std_logic;
+        rst: in std_logic;
+
+        data: inout std_logic_vector(31 downto 0);
+        interrupt: out std_logic;
+        error_success: out std_logic
+    );
+end entity; 
+
+architecture rtl of io is
+begin
+    -- TODO
+end architecture;
---
+++ test/io_tb.vhdl
@@ -0,0 +1,42 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library vunit_lib;
+context vunit_lib.vunit_context;
+
+entity io_tb is
+    generic (runner_cfg: string);
+end entity; 
+
+architecture tb of io_tb is
+    constant CLK_FREQ: integer := 100e6; -- 100 MHz
+    constant CLK_PERD: time    := 1000 ms / CLK_FREQ;
+
+    signal clk: std_logic := '0';
+
+    signal in_state: std_logic_vector(1 downto 0);
+    signal rst: std_logic := '0';
+
+    signal data: std_logic_vector(31 downto 0);
+    signal interrupt: std_logic;
+    signal error_success: std_logic;
+begin
+    clk <= not clk after CLK_PERD / 2;
+
+    io: entity work.io port map (in_state => in_state, clk => clk, rst => rst, data => data, interrupt => interrupt, error_success => error_success);
+
+    main: process
+    begin
+        test_runner_setup(runner, runner_cfg);
+        set_stop_level(failure);
+
+        if run("test_case_name") then
+            -- TODO
+        end if;
+
+        wait for CLK_PERD/2;
+        test_runner_cleanup(runner);
+        wait;
+    end process;
+end architecture;
