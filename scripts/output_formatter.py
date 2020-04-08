7117904f90861cbd7f72b76c52194243a04f7781
Mahmoud Adas <mido3ds@gmail.com>
2020-04-08 14:44:39 +0200

    verify output

--- scripts/output_formatter.py
+++ scripts/output_formatter.py
@@ -1,5 +1,9 @@
-# input simulation output
-# output json file format of the simulation output
+'''
+input simulation output
+output json file format of the simulation output
+
+$ python3 output_formatter.py <path/to/input.sout >path/to/output.json
+'''
 
 # TODO
 # read input binary
--- scripts/preprocessor.py
+++ scripts/preprocessor.py
@@ -1,11 +1,48 @@
-# read input json
-# ouput simulation input ready file
+'''
+read input json
+ouput simulation input ready file
+
+$ python3 preprocessor.py <path/to/input.json >path/to/output.sin
+'''
+import json
+import sys
 
 # TODO
 # read file
-# verify schema
-# verify vars
-# prepare IO input
+inp = json.load(sys.stdin)
+
+# verify input
+assert inp['N'] < 2**6 and inp['N'] > 0
+assert inp['M'] < 2**6 and inp['M'] > 0
+assert inp['Count'] <= 5 and inp['Count'] > 0
+
+assert len(inp['A']) == inp['N']
+for a in inp['A']:
+    assert len(a) == inp['N'], \
+        f'a={a}, inp["N"]={inp["N"]}'
+
+assert len(inp['B']) == inp['N']
+for b in inp['B']:
+    assert len(b) == inp['M'], \
+        f'b={b}, inp["M"]={inp["M"]}'
+
+assert len(inp['Us']) == inp['M']
+for u in inp['Us']:
+    assert len(u) == inp['Count'], \
+        f'u={u}, inp["Count"]={inp["Count"]}'
+
+assert len(inp['T']) == inp['Count']
+assert len(inp['U0']) == inp['Count']
+assert len(inp['X0']) == inp['N']
+
+assert inp['Mode'] in [1, 2]
+if inp['Mode'] == 1:
+    assert 'H' in inp
+    assert 'Err' in inp
+
+assert inp['Fixedpoint'] in [1, 2, 3]
+
+# prepare simulation input
 # cast numbers to binary stream
 # compress binary stream
 # output compressed data
