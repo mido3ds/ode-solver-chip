d1ecb2ccb918949e0f7f02168d20a1a52252b0c7
Evraa <evramyousef@gmail.com>
2020-04-18 08:04:44 +0200

    SOLVER: pushing for now..

--- src/solver.vhdl
+++ src/solver.vhdl
@@ -47,6 +47,11 @@ architecture rtl of solver is
     signal fpu_add_3_in_1, fpu_add_3_in_2, fpu_add_3_out               : std_logic_vector(MAX_LENGTH - 1 downto 0)  := (others => '0');
     signal done_add_3, err_add_3, zero_add_3, posv_add_3, enable_add_3 : std_logic                                  := '0';
 
+    --FPU SUB 3
+    --signal operation_sig_2                                             : std_logic_vector(1 downto 0)               := "00";
+    signal fpu_sub_1_in_1, fpu_sub_1_in_2, fpu_sub_1_out               : std_logic_vector(MAX_LENGTH - 1 downto 0)  := (others => '0');
+    signal done_sub_1, err_sub_1, zero_sub_1, posv_sub_1, enable_sub_1 : std_logic                                  := '0';
+
     --Memory signals:
     --RD/WR:
     signal h_main_rd, h_main_wr                                        : std_logic                                  := '0';
@@ -111,7 +116,7 @@ architecture rtl of solver is
     --N*N, needed in looping at A
     signal N_N:  integer range 0 to 2500 ;
     --run a and b processes
-    signal run_a_loop, a_high, a_is_read,write_a_coeff : std_logic  := '0';
+    signal run_a_loop, a_high, a_is_read,write_a_coeff, increment_a_address,decrement_a_address : std_logic  := '0';
     signal a_temp : std_logic_vector(MAX_LENGTH-1 downto 0) := (others => '0');
     --signal N_N_temp: integer range 0 to 2500 ;
     --read h
@@ -121,9 +126,11 @@ architecture rtl of solver is
 
     --result of a*H
     signal result_a_temp : std_logic_vector(MAX_LENGTH-1 downto 0) := (others => '0');
+    signal result_b_temp : std_logic_vector(MAX_LENGTH-1 downto 0) := (others => '0');
 
-
-    signal run_b_loop : std_logic  := '0';
+    --run a and b processes
+    signal run_b_loop, b_high, b_is_read,write_b_coeff ,increment_b_address, decrement_a_address: std_logic  := '0';
+    signal b_temp : std_logic_vector(MAX_LENGTH-1 downto 0) := (others => '0');
 
 
 
@@ -191,6 +198,21 @@ begin
             zero      => zero_add_3,
             posv      => posv_add_3,
             add_sub   => thisIsAdder
+        );
+    fpu_sub_1 : entity work.fpu_adder(rtl)
+        port map(
+            clk       => clk,
+            rst       => rst,
+            mode      => mode_sig,
+            enbl      => enable_sub_1,
+            in_a      => fpu_sub_1_in_1,
+            in_b      => fpu_sub_1_in_2,
+            out_c     => fpu_sub_1_out,
+            done      => done_sub_1,
+            err       => err_sub_1,
+            zero      => zero_sub_1,
+            posv      => posv_sub_1,
+            add_sub   => thisIsSub
         );
     --Memo:
     -- h_main--> two (32) regs.
@@ -586,20 +608,23 @@ begin
     end process ; -- FIXED
 
     runA : process(clk, run_a_loop, a_is_read, h_is_read, write_a_coeff)
-        variable proceed:std_logic  := '0';
-        variable with_one: std_logic  := '0';
-        variable first_time: std_logic  := '1';
-        variable N_N_temp:  integer range 0 to 2500 ;
-        begin 
+    variable proceed:std_logic  := '0';
+    variable with_one: std_logic  := '0';
+    variable first_time: std_logic  := '1';
+    variable N_N_temp:  integer range 0 to 2500 ;
+    begin 
         if rising_edge(clk) and run_a_loop = '1' and a_is_read = '1' and h_is_read = '1' then
             if first_time = '1' then
                 N_N_temp := N_N;
+                a_coeff_address<= (others => '0');
                 first_time := '0';
             end if;
             N_N_temp := N_N_temp - 1;
             --we reached the end of the loop            
             if N_N_temp = 0 then 
-                proceed := '0'; 
+                proceed := '0';
+                --EXIT LOOP
+                run_a_loop <= '0';
             else 
                 proceed := '1';
             end if;
@@ -626,6 +651,7 @@ begin
             end if;
 
         end if;
+
     end process ; -- runA
 
     --This process responsible for reading coeff A at addresses [x,x+1]
@@ -634,27 +660,28 @@ begin
     begin
         if rising_edge(clk) and run_a_loop = '1' and a_is_read = '0' and write_a_coeff = '0' then
             if a_high = '0' then
-                --reading the low part
-                a_coeff_rd <= '1';
-                a_temp(63 downto 32) <= a_coeff_data_out;
-                a_high <= '1';
+                if increment_a_address = '0' then
+                    --reading the low part
+                    a_coeff_rd <= '1';
+                    a_temp(63 downto 32) <= a_coeff_data_out;
+                    a_high <= '1';
+                    increment_a_address <= '1';
+                end if;
             else
-                a_coeff_rd <= '1';
-                a_temp(31 downto 0) <= a_coeff_data_out;
-                a_high <= '0';
-                a_is_read <= '1';
+                if increment_a_address = '0' then
+                    a_coeff_rd <= '1';
+                    a_temp(31 downto 0) <= a_coeff_data_out;
+                    a_high <= '0';
+                    a_is_read <= '1';
+                    decrement_a_address <= '1';
+                end if;
             end if;
-            fpu_add_2_in_1 <= a_coeff_address;
-            fpu_add_2_in_2 <= X"0001";
-            enable_add_2 <= '1';
-            a_coeff_address <= fpu_add_2_out;
         end if;
         --rga3 el-7aga zay ma kant!
         a_coeff_rd <= '0';
-        enable_add_2 <= '0';
     end process ; -- read_a_coeff
 
-    read_h_main : process(clk, h_is_read,read_h_please)
+    read_h_main : process(clk, h_is_read, read_h_please)
     begin
         if rising_edge(clk) and h_is_read = '0' and read_h_please='1' then
             if h_high = '0' then
@@ -677,6 +704,9 @@ begin
         h_main_rd <= '0';
         enable_add_3 <= '0';
     end process ; -- read_a_coeff
+
+
+    
     --PROCESS to write result_A_temp --> A[i,i+1]
     write_a : process(clk, run_a_loop, write_a_coeff )
     begin
@@ -684,21 +714,181 @@ begin
         -- we can't read and write at the same time, so add_2 is free
         if rising_edge(clk) and run_a_loop = '1' and write_a_coeff = '1' then
             if a_high = '0' then
-                a_coeff_wr <= '1';
-                a_coeff_data_in <= result_a_temp (63 downto 32) ;
-                a_high <= '1';
+                if decrement_a_address = '0' then 
+                    a_coeff_wr <= '1';
+                    a_coeff_data_in <= result_a_temp (63 downto 32) ;
+                    a_high <= '1';
+                    increment_a_address <= '1';
+                end if;
             else
-                a_coeff_wr <= '1';
-                a_coeff_data_in <= result_a_temp (31 downto 0) ;
-                a_high <= '0';
+                if increment_a_address = '0' then
+                    a_coeff_wr <= '1';
+                    a_coeff_data_in <= result_a_temp (31 downto 0) ;
+                    a_high <= '0';
+                    increment_a_address <= '1';
+                end if;
             end if;
-            fpu_add_2_in_1 <= a_coeff_address;
-            fpu_add_2_in_2 <= X"0001";
-            enable_add_2 <= '1';
-            a_coeff_address <= fpu_add_2_out;
         end if;
         a_coeff_wr <= '0';
-        enable_add_2 <= '0';
     end process ; -- write_a_coeff
+
+    run_h_b : process( clk, run_a_loop,run_b_loop, b_is_read, h_is_read )
+    variable proceed:std_logic  := '0';
+    variable first_time: std_logic  := '1';
+    variable N_M_temp:  integer range 0 to 2500 ;
+    
+    begin
+        -- I will run when:
+        -- clk, I have the right, A is finished, next element is read, h is read.
+        if rising_edge(clk) and run_b_loop ='1' and run_a_loop ='0' and b_is_read='1' and h_is_read ='1' then
+            if first_time = '1' then
+                N_M_temp := N_M;
+                first_time := '0';
+                b_coeff_address <= (others => '0');
+            end if;
+            N_M_temp := N_M_temp - 1;
+            --we reached the end of the loop            
+            if N_N_temp = 0 then 
+                proceed := '0';
+                --EXIT LOOP
+                run_b_loop <= '0';
+            else 
+                proceed := '1';
+            end if;
+
+            if proceed = '1' then
+                fpu_mul_1_in_1 <= b_temp;
+                fpu_mul_1_in_2 <= h_temp;
+                enable_mul_1 <= '1';
+                -- we will wait till writing :'(
+                result_b_temp <= fpu_mul_1_out;
+                write_b_coeff <= '1';
+                b_is_read <= '0'; --to read another A                
+            end if;
+        end if;
+    end process ; -- run_h_b
+
+
+    read_b_coeff : process(clk, run_b_loop, b_is_read, write_b_coeff)
+    begin
+        if rising_edge(clk) and run_b_loop = '1' and b_is_read = '0' and write_b_coeff = '0' then
+            if b_high = '0' then
+                if increment_b_address = '0' then
+                    --reading the low part
+                    b_coeff_rd <= '1';
+                    b_temp(63 downto 32) <= b_coeff_data_out;
+                    b_high <= '1';
+                    increment_b_address <= '1';
+                end if;
+            else
+                if increment_b_address = '0' then
+                    b_coeff_rd <= '1';
+                    b_temp(31 downto 0) <= b_coeff_data_out;
+                    b_high <= '0';
+                    b_is_read <= '1';
+                    decrement_b_address <= '1';
+                end if;
+            end if;
+        end if;
+        --rga3 el-7aga zay ma kant!
+        b_coeff_rd <= '0';
+    end process ; -- read_a_coeff
+
+
+    write_b : process(clk, run_b_loop, write_b_coeff)
+    begin
+        -- result is at result_a_temp
+        -- we can't read and write at the same time, so add_2 is free
+        if rising_edge(clk) and run_b_loop = '1' and write_b_coeff = '1' then
+            if b_high = '0' then
+                if decrement_b_address = '0' then
+                    b_coeff_wr <= '1';
+                    b_coeff_data_in <= result_b_temp (63 downto 32) ;
+                    b_high <= '1';
+                    increment_b_address <= '1';
+                end if;
+            else
+                if increment_b_address = '0' then 
+                    b_coeff_wr <= '1';
+                    b_coeff_data_in <= result_b_temp (31 downto 0) ;
+                    b_high <= '0';
+                    increment_b_address <= '1';
+                end if;
+            end if;
+        end if;
+        b_coeff_wr <= '0';
+    end process ; -- write_a_coeff
+
+
+
+    inc_a_address : process( clk, increment_a_address, done_add_2 )
+    begin
+        if rising_edge (clk) and increment_a_address = '1' then
+            if done_add_2 = '0' then
+                fpu_add_2_in_1 <= a_coeff_address;
+                fpu_add_2_in_2 <= X"0001";
+                enable_add_2 <= '1';
+            else
+                a_coeff_address <= fpu_add_2_out;
+                enable_add_2 <= '0';
+                increment_a_address <='0';
+            end if;
+        end if;
+    end process ; -- inc_a_address
+
+
+    dec_a_address : process( clk, decrement_a_address, done_sub_1 )
+    begin
+        if rising_edge (clk) and decrement_a_address = '1' then
+            if done_sub_1 = '0' then 
+                fpu_sub_1_in_1 <= a_coeff_address;
+                fpu_sub_1_in_2 <= X"0001";
+                enable_sub_1 <= '1';
+            else
+                a_coeff_address <= fpu_add_2_out;
+                enable_sub_1 <= '0';
+                decrement_a_address <='0';
+                
+            end if;
+        end if;
+    end process ; -- dec_a_address
+
+    inc_b_address : process( clk, increment_b_address,done_add_2 )
+    begin
+        if rising_edge (clk) and increment_b_address = '1' then
+            if done_add_2 = '0' then
+                fpu_add_2_in_1 <= b_coeff_address;
+                fpu_add_2_in_2 <= X"0001";
+                enable_add_2 <= '1';
+            else
+                b_coeff_address <= fpu_add_2_out;
+                enable_add_2 <= '0';
+                increment_b_address <='0';
+                
+            end if;
+        end if;
+    end process ; -- inc_b_address
+
+
+    dec_b_address : process( clk, decrement_b_address, done_sub_1)
+    begin
+        if rising_edge (clk) and decrement_b_address = '1' then
+            if done_sub_1 = '0' then
+                fpu_sub_1_in_1 <= b_coeff_address;
+                fpu_sub_1_in_2 <= X"0001";
+                enable_sub_1 <= '1'; 
+            else
+                b_coeff_address <= fpu_add_2_out;
+                enable_sub_1 <= '0';
+                decrement_b_address <='0';
+                
+            end if;
+        end if;
+    end process ; -- dec_b_address
+
+
+
+
+
 
 end architecture;
