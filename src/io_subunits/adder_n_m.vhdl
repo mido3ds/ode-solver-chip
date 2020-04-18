2bc721c449f11a0a60b62135172ce98ec218d1b9
Mahmoud Adas <mido3ds@gmail.com>
2020-04-18 22:00:43 +0200

    adder_n_m.vhdl

--- src/io_subunits/adder_n_m.vhdl
+++ src/io_subunits/adder_n_m.vhdl
@@ -3,7 +3,9 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity adder_n_m is
-    -- N <= M
+    -- N: bits of the input A, output C
+    -- M: bits of input B
+    -- N >= M
     generic (
         N : integer := 7;
         M : integer := 3
@@ -18,7 +20,7 @@ entity adder_n_m is
 end entity;
 
 architecture rtl of adder_n_m is
-    signal adder_b      : std_logic_vector(N - 1 downto 0);
+    signal adder_b      : std_logic_vector(N - 1 downto 0) := (others => '0');
     signal adder_carrys : std_logic_vector(N downto 0);
 begin
     full_adder_0 : for i in 0 to N - 1 generate
@@ -30,7 +32,6 @@ begin
     end generate;
 
     adder_b(M - 1 downto 0) <= b;
-    adder_b(N - 1 downto 3) <= (others => '0');
 
     -- cin
     adder_carrys(0)         <= cin;
