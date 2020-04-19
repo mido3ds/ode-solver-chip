ce43ed5bf7a52c6220f97f188a2e1d3850748406
Mahmoud Adas <mido3ds@gmail.com>
2020-04-19 06:55:48 +0200

    added decmopressor to io

--- src/io.vhdl
+++ src/io.vhdl
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.common.all;
 
 entity io is
     port (
@@ -18,6 +19,44 @@ entity io is
 end entity;
 
 architecture rtl of io is
+    -- decompressor
+    signal dcm_out_data  : std_logic_vector(in_data'range);
+    signal dcm_out_ready : std_logic;
 begin
-    -- TODO
+    decompressor : entity work.decompressor
+        port map(
+            in_data   => cpu_data,
+            rst       => rst,
+            enbl_in   => to_std_logic(in_state = STATE_LOAD),
+            clk       => clk,
+            out_data  => dcm_out_data,
+            out_ready => dcm_out_ready
+        );
+
+    process (clk, rst)
+    begin
+        if rst = '1' then
+            interrupt     <= '0';
+            error_success <= '0';
+            adr           <= (others => 'Z');
+            cpu_data      <= (others => 'Z');
+            in_data       <= (others => 'Z');
+        elsif rising_edge(clk) then
+            case(in_state) is
+                when STATE_LOAD =>
+                null; -- TODO: load logic
+
+                when STATE_WAIT =>
+                null; -- TODO: wait logic
+
+                when STATE_OUT =>
+                null; -- TODO: output logic
+
+                when others        =>
+                -- in other states, we don't intervent in the internal bus
+                in_data <= (others => 'Z');
+                adr     <= (others => 'Z');
+            end case;
+        end if;
+    end process;
 end architecture;
