7805e7462c6f0dbd5698ad85813778328fc8b279
Mahmoud Adas <mido3ds@gmail.com>
2020-04-19 06:22:07 +0200

    fixed decompressor

--- test/io_subunits/decompressor_tb.vhdl
+++ test/io_subunits/decompressor_tb.vhdl
@@ -18,7 +18,7 @@ architecture tb of decompressor_tb is
 
     signal in_data    : std_logic_vector(31 downto 0);
     signal rst        : std_logic;
-    signal enbl       : std_logic;
+    signal enbl_in    : std_logic;
 
     signal out_data   : std_logic_vector(31 downto 0);
     signal buf        : std_logic_vector(128 - 1 downto 0);
@@ -30,7 +30,7 @@ begin
         port map(
             in_data   => in_data,
             rst       => rst,
-            enbl      => enbl,
+            enbl_in   => enbl_in,
             clk       => clk,
             out_data  => out_data,
             buf_dbg   => buf,
@@ -64,95 +64,162 @@ begin
         test_runner_setup(runner, runner_cfg);
         set_stop_level(failure);
 
-        rst     <= '1';
-        enbl    <= '1';
-        in_data <= (others => '0');
-        wait for CLK_PERD;
-        rst <= '0';
+        rst <= '1';
+        wait for 1 ps;
+        enbl_in <= '1';
+        rst     <= '0';
 
-        -- TODO: test data_out, out_ready
+        if run("enbl_in") then
+            fill_64bit(0);
 
-        if run("enbl") then
-            fill_24bit_arbitrary;
-            wait for CLK_PERD * 2;
-            check(out_data /= to_vec('Z', out_data'length));
+            wait until falling_edge(clk);
+            enbl_in <= '0';
+            check_equal(out_ready, '0', "out_ready shouldn't be ready now");
 
-            enbl <= '0';
-            wait for 1 ps;
-            check(out_data = to_vec('Z', out_data'length));
+            wait until falling_edge(clk);
+            check_equal(out_ready, '1', "out_ready should be ready now");
+
+            wait until falling_edge(clk);
+            check_equal(out_ready, '1', "out_ready should be ready now");
+
+            wait until falling_edge(clk);
+            check_equal(out_ready, '0', "should receive no more data");
         end if;
 
         if run("basic_buf_fill") then
             fill_24bit_arbitrary;
-            wait for CLK_PERD;
-            check_equal(flip(buf(23 downto 0)), to_vec("111011111111100111111100", 24));
+            wait until falling_edge(clk);
+            enbl_in <= '0';
+            check_equal(flip(buf(23 downto 0)), to_vec("111011111111100111111100", 24), "buffer");
+            check_equal(out_ready, '0', "out_ready shouldn't be ready now");
         end if;
 
         if run("basic_buf_fill_repeated") then
             fill_24bit_arbitrary;
-            wait for CLK_PERD;
-            check_equal(flip(buf(23 downto 0)), to_vec("111011111111100111111100", 24));
+            wait until falling_edge(clk);
+            check_equal(flip(buf(23 downto 0)), to_vec("111011111111100111111100", 24), "buffer");
+            check_equal(out_ready, '0', "out_ready shouldn't be ready now");
 
-            wait for CLK_PERD;
-            check_equal(flip(buf(23 + 24 downto 0)), to_vec("111011111111100111111100" & "111011111111100111111100", 24 + 24));
+            wait until falling_edge(clk);
+            check_equal(flip(buf(23 + 24 downto 0)), to_vec("111011111111100111111100" & "111011111111100111111100", 24 + 24), "buffer");
+            check_equal(out_ready, '0', "out_ready shouldn't be ready now");
 
-            wait for CLK_PERD;
-            check_equal(flip(buf(23 + 24 + 24 downto 0)), to_vec("111011111111100111111100" & "111011111111100111111100" & "111011111111100111111100", 24 + 24 + 24));
+            wait until falling_edge(clk);
+            enbl_in <= '0';
+            check_equal(flip(buf(23 + 24 + 24 downto 0)), to_vec("111011111111100111111100" & "111011111111100111111100" & "111011111111100111111100", 24 + 24 + 24), "buffer");
+            check_equal(out_ready, '1', "out_ready");
+            check_equal(out_data, to_vec("11101111111110011111110011101111", 32), "out_data");
         end if;
 
         if run("fill_buf_64bits") then
             fill_64bit(0);
-            wait for CLK_PERD;
-            check_equal(flip(buf(63 downto 0)), to_vec('0', 64));
+            wait until falling_edge(clk);
+            enbl_in <= '0';
+            check_equal(flip(buf(63 downto 0)), to_vec('0', 64), "buffer");
+            check_equal(out_ready, '0', "out_ready shouldn't be ready now");
+
+            wait until falling_edge(clk);
+            check_equal(out_ready, '1', "out_ready should be ready now");
+            check_equal(out_data, to_vec('0', 32), "out_data");
         end if;
 
         if run("fill_buf_96bits") then
+            -- [0]
             fill_64bit(0);
-            wait for CLK_PERD;
-            check_equal(flip(buf(63 downto 0)), to_vec('0', 64));
+            wait until falling_edge(clk);
+            check_equal(flip(buf(63 downto 0)), to_vec('0', 64), "buffer");
+            check_equal(out_ready, '0', "out_ready shouldn't be ready now");
 
+            -- [1]
             fill_32bit(1);
-            wait for CLK_PERD;
-            check_equal(flip(buf(96 - 1 downto 0)), to_vec('0', 64) & to_vec('1', 32));
+            wait until falling_edge(clk);
+            enbl_in <= '0';
+            check_equal(flip(buf(96 - 1 downto 0)), to_vec('0', 64) & to_vec('1', 32), "buffer");
+            check_equal(out_ready, '1', "out_ready should be ready now"); -- [0]
+            check_equal(out_data, to_vec('0', 32), "out_data");
+
+            wait until falling_edge(clk);
+            check_equal(out_ready, '1', "out_ready should be ready now"); -- [0]
+            check_equal(out_data, to_vec('0', 32), "out_data");
+
+            wait until falling_edge(clk);
+            check_equal(out_ready, '1', "out_ready should be ready now"); -- [1]
+            check_equal(out_data, to_vec('1', 32), "out_data");
+
+            wait until falling_edge(clk);
+            check_equal(out_ready, '0', "out_ready shouldn't be ready now");
         end if;
 
         if run("fill_buf_all") then
-            -- 64 zeros
+            --[0]
             fill_64bit(0);
-            wait for CLK_PERD;
-            check_equal(flip(buf(63 downto 0)), to_vec('0', 64));
+            wait until falling_edge(clk);
+            check_equal(flip(buf(63 downto 0)), to_vec('0', 64), "buffer");
+            check_equal(out_ready, '0', "out_ready shouldn't be ready now");
 
-            -- 32 ones
+            --[1]
             fill_32bit(1);
-            wait for CLK_PERD;
-            check_equal(flip(buf(96 - 1 downto 0)), to_vec('0', 64) & to_vec('1', 32));
+            wait until falling_edge(clk);
+            check_equal(flip(buf(96 - 1 downto 0)), to_vec('0', 64) & to_vec('1', 32), "buffer");
+            check_equal(out_ready, '1', "out_ready should be ready now"); -- [0]
+            check_equal(out_data, to_vec('0', 32), "out_data");
 
-            -- 32 zeros
+            --[2]
             fill_32bit(0);
-            wait for CLK_PERD;
-            check_equal(flip(buf(128 - 1 downto 0)), to_vec('0', 64) & to_vec('1', 32) & to_vec('0', 32));
+            wait until falling_edge(clk);
+            enbl_in <= '0';
+            check_equal(flip(buf(128 - 1 downto 0)), to_vec('0', 64) & to_vec('1', 32) & to_vec('0', 32), "buffer");
+            check_equal(out_ready, '1', "out_ready should be ready now"); -- [0]
+            check_equal(out_data, to_vec('0', 32), "out_data");
+
+            wait until falling_edge(clk);
+            check_equal(out_ready, '1', "out_ready should be ready now"); -- [1]
+            check_equal(out_data, to_vec('1', 32), "out_data");
+
+            wait until falling_edge(clk);
+            check_equal(out_ready, '1', "out_ready should be ready now"); -- [2]
+            check_equal(out_data, to_vec('0', 32), "out_data");
+
+            wait until falling_edge(clk);
+            wait until falling_edge(clk);
+            check_equal(out_ready, '0', "out_ready shouldn't be ready now");
         end if;
 
         if run("fill_buf_wrap") then
-            -- 64 zeros
+            -- [0]
             fill_64bit(0);
-            wait for CLK_PERD;
-            check_equal(flip(buf(63 downto 0)), to_vec('0', 64));
+            wait until falling_edge(clk);
+            check_equal(flip(buf(63 downto 0)), to_vec('0', 64), "buffer");
+            check_equal(out_ready, '0', "out_ready shouldn't be ready now");
 
-            -- 32 ones
+            -- [1]
             fill_32bit(1);
-            wait for CLK_PERD;
-            check_equal(flip(buf(96 - 1 downto 0)), to_vec('0', 64) & to_vec('1', 32));
+            wait until falling_edge(clk);
+            check_equal(flip(buf(96 - 1 downto 0)), to_vec('0', 64) & to_vec('1', 32), "buffer");
+            check_equal(out_ready, '1', "out_ready should be ready now"); -- [0]
+            check_equal(out_data, to_vec('0', 32), "out_data");
 
-            -- 32 zeros
+            -- [2]
             fill_32bit(0);
-            wait for CLK_PERD;
-            check_equal(flip(buf(128 - 1 downto 0)), to_vec('0', 64) & to_vec('1', 32) & to_vec('0', 32));
+            wait until falling_edge(clk);
+            check_equal(flip(buf(128 - 1 downto 0)), to_vec('0', 64) & to_vec('1', 32) & to_vec('0', 32), "buffer");
+            check_equal(out_ready, '1', "out_ready should be ready now"); -- [0]
+            check_equal(out_data, to_vec('0', 32), "out_data");
 
-            -- 24 arbitrary
+            -- [3]
             fill_24bit_arbitrary;
-            wait for CLK_PERD;
-            check_equal(flip(buf), to_vec("111011111111100111111100", 24) & to_vec('0', 64 - 24) & to_vec('1', 32) & to_vec('0', 32));
+            wait until falling_edge(clk);
+            enbl_in <= '0';
+            check_equal(flip(buf), to_vec("111011111111100111111100", 24) & to_vec('0', 64 - 24) & to_vec('1', 32) & to_vec('0', 32), "buffer");
+            check_equal(out_ready, '1', "out_ready should be ready now"); -- [1]
+            check_equal(out_data, to_vec('1', 32), "out_data");
+
+            wait until falling_edge(clk);
+            check_equal(out_ready, '1', "out_ready should be ready now"); -- [2]
+            check_equal(out_data, to_vec('0', 32), "out_data");
+
+            wait until falling_edge(clk);
+            check_equal(out_ready, '0', "out_ready shouldn't be ready now"); -- [4]
         end if;
 
         wait for CLK_PERD/2;
