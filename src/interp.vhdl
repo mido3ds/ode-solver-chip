9744e0c5bd85a3f69a205dcac67487937e186f8b
darkgeek <mohamedshawky911@gmail.com>
2020-04-20 21:17:27 +0200

    Interp: added main FSM

--- src/interp.vhdl
+++ src/interp.vhdl
@@ -84,9 +84,21 @@ signal U_out_data_in, U_out_data_out : std_logic_vector(WORD_LENGTH - 1 downto 0
 --Processes Signals
 --Main FSM Signals
 signal interp_state : std_logic_vector(3 downto 0) := (others => '0');
+signal t_low, t_high : std_logic_vector(MAX_LENGTH - 1 downto 0) := (others => '0'); --range boundaries
+signal t_const : std_logic_vector(MAX_LENGTH - 1 downto 0) := (others => '0'); --(Tk-Tn)/(Tz-Tn)
+signal u_low_adr, u_high_adr : std_logic_vector(9 downto 0) := (others => '0'); --boundary Us addresses
+signal u_low_temp, u_high_temp : std_logic_vector(MAX_LENGTH - 1 downto 0) := (others => '0'); --boundary Us values
+signal u_out_result : std_logic_vector(MAX_LENGTH - 1 downto 0) := (others => '0'); --result of Uout
 
 --Range Finder Signals
 signal range_finder_enable : std_logic := '0';
+
+--Us IO Signals
+signal read_u_s_low, write_u_s_low, increment_u_s_low, decrement_u_s_low : std_logic := '0';
+signal read_u_s_high, write_u_s_high, increment_u_s_high, decrement_u_s_high : std_logic := '0';
+
+--Uout IO Signals
+signal read_u_out, write_u_out, increment_u_out, decrement_u_out : std_logic := '0';
 
 begin
 -----------------------------------------------------------------PORT MAPS-----------------------------------------------------------------------------------
@@ -236,8 +248,106 @@ begin
         null;
     end process;
 -----------------------------------------------------------------MEMORY IO-----------------------------------------------------------------------------------
+    --U_s
+    --reads low Us
+    read_low_us : process(clk, read_u_s_low, write_u_s_low)
+    begin
+        if rst = '0' and rising_edge(clk) and read_u_s_low = '1' and write_u_s_low = '0' then
+            null;            
+    end if;
+    end process; 
+
+    --writes low Us
+    write_low_us : process(clk, read_u_s_low, write_u_s_low)
+    begin
+        if rst = '0' and rising_edge(clk) and read_u_s_low = '0' and write_u_s_low = '1' then       
+            null;
+        end if;
+    end process;
+
+    --increments low Us address
+    inc_low_us : process(clk, increment_u_s_low)
+    begin
+        if rst = '0' and rising_edge(clk) and increment_u_s_low = '1' then
+            null;
+        end if;    
+    end process;
+
+    --decrements low Us address
+    dec_low_us : process(clk, decrement_u_s_low)
+    begin
+        if rst = '0' and rising_edge(clk) and decrement_u_s_low = '1' then
+            null;
+        end if;    
+    end process;
+
+    --reads high Us
+    read_high_us : process(clk, read_u_s_high, write_u_s_high)
+    begin
+        if rst = '0' and rising_edge(clk) and read_u_s_high = '1' and write_u_s_high = '0' then
+            null;            
+    end if;
+    end process; 
+
+    --writes high Us
+    write_high_us : process(clk, read_u_s_high, write_u_s_high)
+    begin
+        if rst = '0' and rising_edge(clk) and read_u_s_high = '0' and write_u_s_high = '1' then       
+            null;
+        end if;
+    end process ;
+
+    --increments high Us address
+    inc_high_us : process(clk, increment_u_s_high)
+    begin
+        if rst = '0' and rising_edge(clk) and increment_u_s_high = '1' then
+            null;
+        end if;    
+    end process ;
+
+    --decrements high Us address
+    dec_high_us : process(clk, decrement_u_s_high)
+    begin
+        if rst = '0' and rising_edge(clk) and decrement_u_s_high = '1' then
+            null;
+        end if;    
+    end process ;
+
+    --U_out
+    --reads Uout
+    read_uout : process(clk, read_u_out, write_u_out)
+    begin
+        if rst = '0' and rising_edge(clk) and read_u_out = '1' and write_u_out = '0' then
+            null;            
+    end if;
+    end process ; 
+
+    --writes Uout
+    write_uout : process(clk, read_u_out, write_u_out)
+    begin
+        if rst = '0' and rising_edge(clk) and read_u_out = '0' and write_u_out = '1' then       
+            null;
+        end if;
+    end process ;
+
+    --increments Uout address
+    inc_uout : process(clk, increment_u_out)
+    begin
+        if rst = '0' and rising_edge(clk) and increment_u_out = '1' then
+            null;
+        end if;    
+    end process ;
+
+    --decrements Uout address
+    dec_uout : process(clk, decrement_u_out)
+    begin
+        if rst = '0' and rising_edge(clk) and decrement_u_out = '1' then
+            null;
+        end if;    
+    end process ;
 -----------------------------------------------------------------MATRIX MANIPULATION-----------------------------------------------------------------------------------
 -----------------------------------------------------------------UTILITIES-----------------------------------------------------------------------------------
+    --finds the range in which the received T lies
     range_finder : process(clk, range_finder_enable)
         begin
             if rst = '0' and rising_edge(clk) and range_finder_enable = '1' then
@@ -258,8 +368,67 @@ begin
                         interp_state <= "0001";
                     end if;
                 when "0001" =>
+                    --read higher part of h_new
+                    --start range finder process
                     h_new(63 downto 32) <= in_data;
-
+                    range_finder_enable <= '1';
+                    interp_state <= "0010";
+                when "0010" =>
+                    --check range finder completion
+                    --subtract Tz-Tn and Tk-Tn
+                    if range_finder_enable = '0' then
+                        fpu_sub_1_in_1 <= t_high;
+                        fpu_sub_1_in_2 <= t_low;
+                        enable_sub_1 <= '1';
+                        fpu_sub_2_in_1 <= h_new;
+                        fpu_sub_2_in_2 <= t_low;
+                        enable_sub_2 <= '1';
+                        interp_state <= "0011";
+                    end if;
+                when "0011" =>
+                    if done_sub_1 = '1' and done_sub_2 = '1' then
+                        fpu_div_1_in_1 <= fpu_sub_2_out;
+                        fpu_div_1_in_2 <= fpu_sub_1_out;
+                        enable_div_1 <= '1';
+                        interp_state <= "0100";
+                    end if;
+                when "0100" =>
+                    if done_div_1 = '1' then
+                        t_const <= fpu_div_1_out;
+                        read_u_s_low <= '1';
+                        interp_state <= "0101";
+                    end if;
+                when "0101" =>
+                    if read_u_s_low = '0' then
+                        read_u_s_high <= '1';
+                        interp_state <= "0110";
+                    end if;
+                when "0110" =>
+                    if read_u_s_high = '0' then
+                        fpu_sub_1_in_1 <= u_high_temp;
+                        fpu_sub_1_in_2 <= u_low_temp;
+                        enable_sub_1 <= '1';
+                        interp_state <= "0111";
+                    end if;
+                when "0111" =>
+                    if done_sub_1 = '1' then
+                        fpu_mul_1_in_1 <= fpu_sub_1_out;
+                        fpu_mul_1_in_2 <= t_const;
+                        enable_mul_1 <= '1';
+                        interp_state <= "1000";
+                    end if;
+                when "1000" =>
+                    if done_mul_1 = '1' then
+                        fpu_add_1_in_1 <= fpu_mul_1_out;
+                        fpu_add_1_in_2 <= u_low_temp;
+                        enable_add_1 <= '1';
+                        interp_state <= "1001";
+                    end if;
+                when "1001" =>
+                    if done_add_1 = '1' then
+                        u_out_result <= fpu_add_1_out;
+                        write_u_out <= '1';
+                    end if;
                 when others =>
                     --NOP
                     null;
